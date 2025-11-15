52bb02681edcfb7632d53545ce632f74cc07b744
wassim <wassimoucouc2@gmail.com>
2025-11-15 20:35:28 +0100

    jenkins test

--- src/test/java/org/example/neosupply/NeoSupplyApplicationTests.java
+++ src/test/java/org/example/neosupply/NeoSupplyApplicationTests.java
@@ -2,8 +2,8 @@ package org.example.neosupply;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
 class NeoSupplyApplicationTests {
 
