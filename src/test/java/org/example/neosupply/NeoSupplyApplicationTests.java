ce7dec3a0f2b14010edf0f4cdc20e4be3f1a4985
wassim <wassimoucouc2@gmail.com>
2025-11-16 15:01:31 +0100

    jenkins test

--- src/test/java/org/example/neosupply/NeoSupplyApplicationTests.java
+++ src/test/java/org/example/neosupply/NeoSupplyApplicationTests.java
@@ -3,8 +3,19 @@ package org.example.neosupply;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.properties.hibernate.query.mutation_strategy=org.hibernate.query.sqm.mutation.internal.inline.InlineMutationStrategy",
+        "spring.data.jdbc.repositories.enabled=false"
+})
 class NeoSupplyApplicationTests {
 
     @Test
