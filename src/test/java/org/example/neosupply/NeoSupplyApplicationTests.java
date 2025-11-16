c5bdda6bd5d4363082af2251d1e1785cfa90e0d5
wassim <wassimoucouc2@gmail.com>
2025-11-16 15:07:55 +0100

    jenkins test

--- src/test/java/org/example/neosupply/NeoSupplyApplicationTests.java
+++ src/test/java/org/example/neosupply/NeoSupplyApplicationTests.java
@@ -1,21 +1,14 @@
 package org.example.neosupply;
 
+import org.example.neosupply.config.TestDatabaseConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
-        "spring.datasource.driverClassName=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
-        "spring.jpa.hibernate.ddl-auto=create-drop",
-        "spring.jpa.properties.hibernate.query.mutation_strategy=org.hibernate.query.sqm.mutation.internal.inline.InlineMutationStrategy",
-        "spring.data.jdbc.repositories.enabled=false"
-})
+@Import(TestDatabaseConfiguration.class)
 class NeoSupplyApplicationTests {
 
     @Test
